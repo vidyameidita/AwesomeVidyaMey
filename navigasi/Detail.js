0bec6ca98507cd4643ec500b75ade09bf9c9211f
vidyameidita <92900749+vidyameidita@users.noreply.github.com>
2022-11-19 20:02:53 +0700

    Update Detail.js

--- navigasi/Detail.js
+++ navigasi/Detail.js
@@ -2,8 +2,9 @@ import * as React from 'react';
 import {View, Text, StyleSheet, Button, Image, Linking} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import { Title, Card } from 'react-native-paper';
-// import {MaterialIcons, Entypo} from 'react-native-vector-icons';
-import karyawan from './Karyawan';
+import email from '../icon/email.png';
+import call from '../icon/phone-call.png';
+import dollar from '../icon/dollar-currency-symbol.png';
 
 
 
@@ -14,7 +15,7 @@ const Detail = ({navigation, route}) => {
             <LinearGradient colors={["#0033ff", "#6bc1ff"]} style={{height:"20%"}}/>
             <View style={{alignItems:"center"}}>
                 <Image style={{width:120, height:120, borderRadius:120/2, marginTop:-50}}
-                source={{uri:karyawan.foto}}/>
+                source={{uri: karyawan.foto}}/>
             </View>
 
             <View style={{alignItems:"center", margin:14}}>
@@ -22,22 +23,22 @@ const Detail = ({navigation, route}) => {
                 <Text style={{fontSize:14}}>{karyawan.jabatan}</Text>
             </View>
             <Card style={{margin:3}}
-            onPress={()=>{Linking.openURL('mailto:${karyawan.email}')}}>
+            onPress={()=>{Linking.openURL('mailto:${karyawan.email}&subject')}}>
                 <View style={{flexDirection:"row", padding:8}}>
-//                     <MaterialIcons name="email" size={32} color="#006aff"/>
+                    <Image source={email} style={{width:30, height:30}}/>
                     <Text style={styles.teks}>{karyawan.email}</Text>
                 </View>
             </Card>
             <Card style={{margin:3}}
             onPress={()=>{Linking.openURL('tel:${karyawan.telp}')}}>
                 <View style={{flexDirection:'row', padding:8}}>
-//                     <Entypo name="phone" size={32} color="#006aff"/>
+                    <Image source={call} style={{width:30, height:30}}/>
                     <Text style={styles.teks}>{karyawan.telp}</Text>
                 </View>
             </Card>
             <Card style={{margin:3}}>
                 <View style={{flexDirection:'row', padding:8}}>
-//                     <MaterialIcons name="attach-money" size={32} color="#006aff"/>
+                    <Image source={dollar} style={{width:30, height:30}}/>
                     <Text style={styles.teks}>{karyawan.gaji}</Text>
                 </View>
             </Card>
