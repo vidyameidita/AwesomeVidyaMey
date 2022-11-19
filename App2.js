3e3993464f5103d46d072d1e39d878bfdb7f75e1
vidyameidita <92900749+vidyameidita@users.noreply.github.com>
2022-11-20 00:15:40 +0700

    Add files via upload

---
+++ App2.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './navigasi/Home';
+import Detail from './navigasi/Detail';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home}
+        options={{
+          title: 'Data Karyawan',
+          headerStyle: { backgroundColor: '#006aff'},
+          headerTintColor: '#fff'
+        }}/>
+        <Stack.Screen name="Detail" component={Detail}
+        options={{
+          title: 'Detail Karyawan',
+          headerStyle: { backgroundColor: '#006aff'},
+          headerTintColor: '#fff'
+        }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
