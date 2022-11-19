8d41b78df1bf13befa9aac7ff4cb315b76b511e3
vidyameidita <92900749+vidyameidita@users.noreply.github.com>
2022-11-20 00:15:19 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,31 +1,44 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ImageBackground, FlatList} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './navigasi/Home';
-import Detail from './navigasi/Detail';
+import { Card } from 'react-native-paper';
+import styles from './styles';
 
-const Stack = createNativeStackNavigator();
 
-function App() {
+export default function App() {
+const [data, setData] = useState([]);
+
+useEffect(()=> {
+  fetch('https://raw.githubusercontent.com/algosigma/js-reactjs/master/homestays.json')
+  .then((response)=> response.json)
+  .then((hasil)=> setData(hasil))
+  .catch(error=> {console.log;})
+
+}, []);
+
+const listHomestay = ({item}) => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home}
-        options={{
-          title: 'Data Karyawan',
-          headerStyle: { backgroundColor: '#006aff'},
-          headerTintColor: '#fff'
-        }}/>
-        <Stack.Screen name="Detail" component={Detail}
-        options={{
-          title: 'Detail Karyawan',
-          headerStyle: { backgroundColor: '#006aff'},
-          headerTintColor: '#fff'
-        }}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    <Card style={styles.cardUtama}>
+      <View style={styles.containerTitle}>
+        <Text style={styles.title}>{item.nama}</Text>
+      </View>
+      <View style={styles.containerImage}>
+        <ImageBackground style={styles.image}
+        source={{uri:item.fotoUr1}}>
+          <Text style={styles.price}>Rp. {item.harga} Rb</Text>
+        </ImageBackground>
+      </View>
+    </Card>
+  )
 }
 
-export default App;
+return (
+  <View style={{flex:1, padding:24}}>
+    <FlatList
+    data={data}
+    keyExtractor={item=>item.id}
+    renderItem={listHomestay}/>
+  </View>
+);
+}
